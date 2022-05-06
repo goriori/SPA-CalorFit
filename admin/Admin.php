6badbaf919e860a9e84ddaebce964f847dcede8c
goriori <100791613+goriori@users.noreply.github.com>
2022-05-06 17:12:21 +0300

    add admin page

---
+++ admin/Admin.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>CalorFit</title>
+</head>
+<body>
+<div class="wrapper">
+      <my-burger />
+      <div class="container">
+        <div class="admin_main">
+          <div>Панель администратора</div>
+          <button class="waves-effect waves-light btn">Изменить пароль</button>
+          <button class="waves-effect waves-light btn">Выйти</button>
+
+          <table border="1">
+            <tr>
+              <th>Номер</th>
+              <th>Почта</th>
+              <th>Сумма оплаты, руб</th>
+              <th>Статус оплаты</th>
+              <th>Файл с питанием</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>email.simple@mail.ru</td>
+              <td>8 560</td>
+              <td>Не оплачено</td>
+              <td>simplesimple.pdf</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
