5ac2dd0061c93d07ff6d6808cece04ef65541027
goriori <100791613+goriori@users.noreply.github.com>
2022-05-06 14:25:24 +0300

    dwd

--- index.php
+++ index.php
@@ -1,17 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
-   
-  
-  <script src="https://cdn.jsdelivr.net/npm/vue@2">
 
-  </script>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="block-one">
+  <div class="container">
+      <header>
+        <div class="logo">
+          <a href="/">CalorFit</a>      
+        </div>
+        
+      </header>
+    </div>
+    <div class="content">
+      <p class="white-text center-align">Давайте составим <br />Ваш профиль</p>
+    </div>
+    <div class="imageTest">
+      <div class="Man">
+        <h1 class="white-text">МУЖЧИНА</h1>
+          <div class="imageman"></div>
+      </div>
+      <div class="Woman">
+        <h1 class="white-text">ЖЕНЩИНА</h1>       
+          <div class="imagewoman"></div>
+      </div>
+    </div>
+    <footer >
+    <div class="about">
+      Продолжая, вы соглашаетесь с <a href="#">Условиями использования</a>,
+      <a href="#"
+        >Политикой<br />
+        конфиденциальности</a
+      >, <a href="#">Условиями подписки</a><br />
+      <a href="#">Отписаться от услуги</a><br />
+      Поддержка: admin@meallforme.ru<br />
+      2021 © All Rights Reserved.
+    </div>
+  </footer>
+  </div>
+  </div>
+  </div>
+
+  
 </body>
+
 </html>
