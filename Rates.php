0746aa33f68aa79feb8582c1f86660ad75207b02
goriori <100791613+goriori@users.noreply.github.com>
2022-05-06 15:18:28 +0300

    up

---
+++ Rates.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CalorFit</title>
+    <link rel="stylesheet" href="/css/rates.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+</head>
+<body>
+<div class="wrapper">
+    <div class="block-gran">
+        <div class="text-content white-text">
+            <h1>Тарифы</h1>
+            <p>
+            Пожалуйста, ознакомьтесь с действующим тарифным планом сервиса по разработке плана питания.
+
+            </p>
+            <p>
+            Cтоимость услуги составляет 1 (один) рубль за получение начального плана питания.
+            </p>
+            <p>
+            В дальнейшем через неделю использования сервиса взимается комиссия сервиса в размере 850 (Восемьсот пятьдесят) рублей за пролонгирование плана питания на 7 дней.
+            </p>
+            <p>
+            В дальнейшем через неделю использования сервиса взимается комиссия сервиса в размере 200 (двухсот) рублей за пролонгирование услуги "калькулятор калорий" на 7 дней.
+            </p>
+        </div>
+    </div>
+
+    <footer>
+        <div class="logo white-text">CalorFit</div>
+        <div class="about">
+          Продолжая, вы соглашаетесь с <a href="#">Условиями использования</a>,
+          <a href="#">Политикой<br />
+            конфиденциальности</a>, <a href="#">Условиями подписки</a><br />
+          <a href="#">Отписаться от услуги</a><br />
+          Поддержка: admin@meallforme.ru<br />
+          2021 © All Rights Reserved.
+        </div>
+      </footer>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,52 +5,110 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="/css/style.css">
+  <title>CalorFit</title>
+  <link rel="stylesheet" href="/css/index.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 
 <body>
   <div class="wrapper">
     <div class="block-one">
-  <div class="container">
-      <header>
-        <div class="logo">
-          <a href="/">CalorFit</a>      
-        </div>
-        
-      </header>
-    </div>
-    <div class="content">
-      <p class="white-text center-align">Давайте составим <br />Ваш профиль</p>
-    </div>
-    <div class="imageTest">
-      <div class="Man">
-        <h1 class="white-text">МУЖЧИНА</h1>
+      <div class="head">
+        <div class="logo">CalorFit</div>
+        <header class="header">
+          <div class="container">
+            <div class="header__body">
+              <div class="header__burger">
+                <span></span>
+              </div>
+              <nav class="header__menu">
+                <ul class="header__list">
+                  <li>
+                    <a href="#" class="header__link">Главная</a>
+                  </li>
+                  <li>
+                    <a href="Unsubscribe.php" class="header__link">Отписаться</a>
+                  </li>
+                  <li>
+                    <a href="Rates.php" class="header__link">Тарифы</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Политика<br> конфенденциальности</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Условия пользования</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Личный кабинет</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+      </div>
+     
+        <!-- <header class="header">
+          <div class="container">
+            <div class="header__body">
+              <div class="header__burger">
+                <span></span>
+              </div>
+              <nav class="header__menu">
+                <ul class="header__list">
+                  <li>
+                    <a href="#" class="header__link">Главная</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Отписаться</a>
+                  </li>
+                  <li>
+                    <a href="Rates.php" class="header__link">Тарифы</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Политика<br> конфенденциальности</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Условия пользования</a>
+                  </li>
+                  <li>
+                    <a href="#" class="header__link">Личный кабинет</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header> -->
+      
+      <div class="content">
+        <p class="white-text center-align">Давайте составим <br />Ваш профиль</p>
+      </div>
+      <div class="imageTest">
+        <div class="Man">
+          <h1 class="white-text">МУЖЧИНА</h1>
           <div class="imageman"></div>
-      </div>
-      <div class="Woman">
-        <h1 class="white-text">ЖЕНЩИНА</h1>       
+        </div>
+        <div class="Woman">
+          <h1 class="white-text">ЖЕНЩИНА</h1>
           <div class="imagewoman"></div>
+        </div>
       </div>
+      <footer>
+        <div class="about">
+          Продолжая, вы соглашаетесь с <a href="#">Условиями использования</a>,
+          <a href="#">Политикой<br />
+            конфиденциальности</a>, <a href="#">Условиями подписки</a><br />
+          <a href="#">Отписаться от услуги</a><br />
+          Поддержка: admin@meallforme.ru<br />
+          2021 © All Rights Reserved.
+        </div>
+      </footer>
     </div>
-    <footer >
-    <div class="about">
-      Продолжая, вы соглашаетесь с <a href="#">Условиями использования</a>,
-      <a href="#"
-        >Политикой<br />
-        конфиденциальности</a
-      >, <a href="#">Условиями подписки</a><br />
-      <a href="#">Отписаться от услуги</a><br />
-      Поддержка: admin@meallforme.ru<br />
-      2021 © All Rights Reserved.
-    </div>
-  </footer>
   </div>
   </div>
-  </div>
-
-  
+  <script src="../js/burger.js"></script>
+ 
 </body>
 
 </html>
