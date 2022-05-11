626e80800ded1c5f6f1e536d2f7acf81b3d717fb
MariaLuna459 <mari.lauxina@mail.ru>
2022-05-11 15:49:48 +0400

    вывод имеилов в админке

--- controllers/connect.php
+++ controllers/connect.php
@@ -1,7 +1,7 @@
 <?php
-$hostname = "31.31.196.115";
-$username = "u1665837_admin";
-$password = "Qwas1234";
+$hostname = "localhost";
+$username = "root";
+$password = "";
 $database = "u1665837_calorfit.ru";
 
 $connect = mysqli_connect($hostname, $username, $password, $database);
