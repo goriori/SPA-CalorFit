0f912422436fd1888aac8c156fbb83efb67791c7
MariaLuna459 <mari.lauxina@mail.ru>
2022-05-11 15:20:49 +0400

    fixes

--- controllers/connect.php
+++ controllers/connect.php
@@ -1,7 +1,7 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
+$hostname = "31.31.196.115";
+$username = "u1665837_admin";
+$password = "Qwas1234";
 $database = "u1665837_calorfit.ru";
 
 $connect = mysqli_connect($hostname, $username, $password, $database);
